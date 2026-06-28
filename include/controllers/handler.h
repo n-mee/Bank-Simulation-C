730390d2a24167750b755af83abf7cbaf89544a5
Jnzl <224951203+n-mee@users.noreply.github.com>
2026-06-28 17:25:21 +0800

    fix: minor fixes in transaction handler change parameters to match the dynamic data stucture instead of passing reciever as parameter we use its id to change direct values my finding the exact id in the memory

--- include/controllers/handler.h
+++ include/controllers/handler.h
@@ -3,9 +3,10 @@
 
 // Transact Handlers
 #include "data/model.h"
+#include "data/database_manager.h"
 void handle_withdraw_request(Account* current_user);
 void handle_deposit_request(Account* current_user);
-void handle_transfer_request(Account* sender, Account* receiver);
+void handle_transfer_request(BankDatabase *db, Account *sender);
 
 //Input Handler
 #endif
