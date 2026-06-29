96e7c008a7c5989d9215c501fe117ecbf5f4734c
Jnzl <224951203+n-mee@users.noreply.github.com>
2026-06-29 23:30:00 +0800

    feat: implemented a pin validation and input validation helper for verification

--- include/utils/validators.h
+++ include/utils/validators.h
@@ -3,5 +3,7 @@
 
 #include <stdbool.h>
 bool is_valid_bal(double amt);
+bool is_valid_pin(const char* target_pin, const char* input_pin);
+bool is_valid_length_input(const char* target);
 
 #endif
--- src/utils/validators.c
+++ src/utils/validators.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 #include "utils/validators.h"
 #include "views/displays.h"
 
@@ -7,6 +8,22 @@
 bool is_valid_bal(double amt) {
     if (amt <= 0) {
         error_bal_msg();
+        return false;
+    }
+    return true;
+}
+
+bool is_valid_pin(const char* target_pin, const char* input_pin) {
+    if (strcmp(target_pin, input_pin) != 0) {
+        invalid_pin_msg();
+        return false;
+    }
+    return true;
+}
+
+bool is_valid_length_input(const char* target) {
+    if (strlen(target) < 2) {
+        invalid_str_size();
         return false;
     }
     return true;
