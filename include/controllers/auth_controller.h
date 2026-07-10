7de7d00c846687eb7eee64e912333acf61b1134a
Jnzl <224951203+n-mee@users.noreply.github.com>
2026-07-10 14:53:35 +0800

    docs: finished descriptions from documentation comments

--- include/controllers/auth_controller.h
+++ include/controllers/auth_controller.h
@@ -1,12 +1,20 @@
 #ifndef AUTH_CONTROLLER_H
 #define AUTH_CONTROLLER_H
 
-// headers
+// Dependency Headers
 #include <stdbool.h>
 #include "../include/data/database_functions.h"
 
-// function declaration
+/**
+ * @brief Handles identity registration menu logic
+ * @param db Main database used to alter account datas
+ */
 void handle_registration(BankDatabase *db);
+/**
+ * @brief Handles the login portal of identity portal
+ * @param db Main database used to access accounts
+ * @param session_user Current active account found in database
+ */
 void handle_login(BankDatabase *db, Account **session_user);
 
 #endif
--- include/controllers/transaction_controller.h
+++ include/controllers/transaction_controller.h
@@ -5,9 +5,23 @@
 #include "data/model.h"
 #include "data/database_functions.h"
 
-// fuction declarations
+/**
+ * @brief Processes account withdrawal after several safety checks
+ * @param current_user Pointer to current active user
+ */
 void handle_withdraw_request(Account* current_user);
+
+/**
+ * @brief Handles account logic with several security checks
+ * @param current_user Pointer to current active user
+ */
 void handle_deposit_request(Account* current_user);
+
+/**
+ * @brief Processing account transfer between 2 accounts after several security checks
+ * @param db Pointer to master database for account reference
+ * @param sender Pointer to current active user who's the sender
+ */
 void handle_transfer_request(BankDatabase *db, Account *sender);
 
 #endif
