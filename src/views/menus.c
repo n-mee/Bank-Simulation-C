0683894bd2789785a4e2eb3399c567839d0cf8be
N_mee <224951203+n-mee@users.noreply.github.com>
2026-07-29 21:46:30 +0800

    featx: Formatting and New Menus Fixed old menu formatting and added new menus for account settings option

--- src/views/menus.c
+++ src/views/menus.c
@@ -42,3 +42,71 @@ void account_menu(void) {
     puts("|                                     |");
     puts("+-------------------------------------+");
 }
+
+void profile_settings(void) {
+    puts("+-------------------------------------+");
+    puts("            Profile Settings           ");
+    puts("+-------------------------------------+");
+    puts("|                                     |");
+    puts("|   [1] Change Username               |");
+    puts("|   [2] Change E-mail                 |");
+    puts("|   [3] Change Pin                    |");
+    puts("|                                     |");
+    puts("|   [0] EXIT                          |");
+    puts("|                                     |");
+    puts("+-------------------------------------+");
+}
+
+void preference_settings(void) {
+    puts("+-------------------------------------+");
+    puts("          Preference Settings          ");
+    puts("+-------------------------------------+");
+    puts("|                                     |");
+    puts("|   [1] Email Notifications           |");
+    puts("|   [2] Push Notifications            |");
+    puts("|   [3] Alert Notifications           |");
+    puts("|                                     |");
+    puts("|   [0] EXIT                          |");
+    puts("|                                     |");
+    puts("+-------------------------------------+");
+}
+
+void alert_pref_settings(void) {
+    puts("+-------------------------------------+");
+    puts("          Alert Notifications          ");
+    puts("+-------------------------------------+");
+    puts("|                                     |");
+    puts("|   [3] Low Balance Alerts            |");
+    puts("|   [4] Large Transaction Alerts      |");
+    puts("|                                     |");
+    puts("|   [0] EXIT                          |");
+    puts("|                                     |");
+    puts("+-------------------------------------+");
+}
+
+void payment_settings(void) {
+    puts("+-------------------------------------+");
+    puts("            Payment Settings           ");
+    puts("+-------------------------------------+");
+    puts("|                                     |");
+    puts("|   [1] Account Status                |");
+    puts("|   [2] Daily Limit                   |");
+    puts("|                                     |");
+    puts("|   [0] EXIT                          |");
+    puts("|                                     |");
+    puts("+-------------------------------------+");
+}
+
+void account_status_menu(void) {
+    puts("+-------------------------------------+");
+    puts("             Account Status            ");
+    puts("+-------------------------------------+");
+    puts("|                                     |");
+    puts("|   [1] Freeze Account                |");
+    puts("|   [2] Unfreeze Account              |");
+    puts("|   [3] Close Account                 |");
+    puts("|                                     |");
+    puts("|   [0] EXIT                          |");
+    puts("|                                     |");
+    puts("+-------------------------------------+");
+}
