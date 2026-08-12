8dadf6b1091fd7b15f5498820e6647680d2c14f9
N_mee <224951203+n-mee@users.noreply.github.com>
2026-08-13 01:41:59 +0800

    feat (logging): Implemented a Write-Ahead Logging - Implemented a function for saving status during transactions - This is to prevent loss of data during transaction by reading logs and returning the amount if the transaction is not committed

--- include/common/file_utility.h
+++ include/common/file_utility.h
@@ -13,11 +13,14 @@
 
 #include <stdio.h>
 #include <errno.h>
+#include <time.h>
 /**
  * @brief Initializes the directory given from parameter
  * @param path String reference for directory to be evaluated
  * @return 0 if successful, -1 if the initialization or creation failed.
  */
 int verify_dir_status(const char* path);
+
+void write_transac_log(const char* status, const char* op, int acc1, int acc2, long long amount);
 
 #endif
