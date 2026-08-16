4b0fd3048ccaca0b8b78b1833e6d57e6b39e8b8e
N_mee <224951203+n-mee@users.noreply.github.com>
2026-08-16 15:39:00 +0800

    chore (ui): fixed formatting in success msg

--- src/cli/display_success_msg.c
+++ src/cli/display_success_msg.c
@@ -3,9 +3,9 @@
 
 void account_operation_success(AccountOperationType type) {
     switch (type) {
-        case UPDATE_PIN: printf("\nSuccessfully updated your PIN!\n"); break;
-        case UPDATE_EMAIL: printf("\nSuccessfully updated your email!\n"); break;
-        case UPDATE_USERNAME: printf("\nSuccessfully updated your username!\n"); break;
+        case UPDATE_PIN: printf("\n[+] Successfully updated your PIN!\n"); break;
+        case UPDATE_EMAIL: printf("\n[+] Successfully updated your email!\n"); break;
+        case UPDATE_USERNAME: printf("\n[+] Successfully updated your username!\n"); break;
         default: break;
     }
 }
@@ -14,20 +14,20 @@ void account_notification_status(AccountNotificationsType type, NotificationsSta
     const char* state_str = (status == NOTIF_SUCCESS_ENABLED) ? "ENABLED" : "DISABLED";
 
     switch (type) {
-        case ENABLE_EMAIL_NOTIF: printf("Email notifications are now %s!\n", state_str); break;
-        case ENABLE_PUSH_NOTIF: printf("Push notifications are now %s!\n", state_str); break;
-        case ENABLE_LARGE_TXN_NOTIF: printf("Large Transaction alerts are now %s!\n", state_str); break;
-        case ENABLE_LOW_BAL_NOTIF: printf("Low Balance alerts are now %s!\n", state_str); break;
+        case ENABLE_EMAIL_NOTIF: printf("[+] Email notifications are now %s!\n", state_str); break;
+        case ENABLE_PUSH_NOTIF: printf("[+] Push notifications are now %s!\n", state_str); break;
+        case ENABLE_LARGE_TXN_NOTIF: printf("[+] Large Transaction alerts are now %s!\n", state_str); break;
+        case ENABLE_LOW_BAL_NOTIF: printf("[+] Low Balance alerts are now %s!\n", state_str); break;
         default: break;
     }
 }
 
 void account_state_success(AccountStateOperationType type) {
     switch (type) {
-        case SET_STATE_FREEZE: printf("Successfully froze your account.\n"); break;
-        case SET_STATE_ACTIVATE: printf("Successfully re-activate your account.\n"); break;
-        case SET_STATE_CLOSED: printf("Successfully set your account as closed.\n"); break;
-        case SET_LIMIT_UPDATE: printf("Successfully updated your account limits.\n"); break;
+        case SET_STATE_FREEZE: printf("[+] Successfully froze your account.\n"); break;
+        case SET_STATE_ACTIVATE: printf("[+] Successfully re-activate your account.\n"); break;
+        case SET_STATE_CLOSED: printf("[+] Successfully set your account as closed.\n"); break;
+        case SET_LIMIT_UPDATE: printf("[+] Successfully updated your account limits.\n"); break;
         default: break;
     }
 }
