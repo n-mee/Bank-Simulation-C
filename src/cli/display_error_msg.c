07be24f1bc1b5b55644b71afd9d9829dbff0b02f
N_mee <224951203+n-mee@users.noreply.github.com>
2026-08-16 16:12:51 +0800

    chore (ui): fixed system err msg - Turned developer style err msg to a user-friendly readable one

--- src/cli/display_error_msg.c
+++ src/cli/display_error_msg.c
@@ -4,7 +4,7 @@
 void invalid_integer_value(ParseExitResult status) {
     switch(status) {
         case ERR_INVALID_INT: printf("[!] ERROR: Input does not start with a number.\n"); break;
-        case ERR_NULL_PTR: printf("[!] ERROR: Input returned a Null pointer.\n"); break;
+        case ERR_NULL_PTR: printf("[!] ERROR: Input cannot be empty.\n"); break;
         case ERR_TRAILING_GARBAGE: printf("[!] ERROR: Found trailing garbage characters after a number.\n"); break;
         default: break;
     }
